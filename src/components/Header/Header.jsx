1a0eec5d0ff265a13d7183e8b304f84df9f6d372
Andy Elaskar <andres_elaskar@hotmail.com>
2022-09-29 14:24:07 -0300

    primera pre entrega

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import logo from "../../logo.png";
+import CartWidget from "../CartWidget/CartWidget";
+import "./header.css";
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <div className="img-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="">Inicio</a>
+            </li>
+            <li>
+              <a href="">Cositas</a>
+            </li>
+            <li>
+              <a href="">Contacto</a>
+            </li>
+            <li className="cart">
+              <a href="">
+                <CartWidget />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
