d304ec8544459c5639b4fcb639476f7e47ca857f
Andy Elaskar <andres_elaskar@hotmail.com>
2022-10-17 08:40:37 -0300

    correcciones segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import Header from './components/Header/Header';
+import React, { Fragment } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <ItemListContainer />
-    </div>
+    <Fragment>
+      <BrowserRouter> 
+      {/* Se ordenan segun la ruta de navegación.
+      Los que se muestran siempre se indican con el componente Routes -condicionales
+      Los que no, van afuera del componente Routes  */}
+        <NavBar />
+
+        <Routes>
+          {/* La parte dinámica la escribo luego de ':' */}
+          < Route path='/' element={<ItemListContainer />} />
+          < Route path='/category/:categoryId' element={<ItemListContainer />} />
+          < Route path='/item/:productId' element={<ItemDetailContainer />} />
+          < Route path='*' element={<h1>404 ERROR NOT FOUND</h1>} />
+        </Routes>
+
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
